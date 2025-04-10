2685872f0cd6d3fdd0ea8d52f3cd08e5b5458508
PrabhaSDET-TestV <prabhakaranr@testvagrant.com>
2025-04-10 16:55:24 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,39 +1,49 @@
 import path from "path";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-    runner: 'local',
-    specs: [
-        './test/specs/android/*.js'
-    ],
-    exclude: [],
-    maxInstances: 10,
-    services: ['appium'],
-    hostname: 'localhost',
-    port: 4723,
-    path: '/wd/hub',
+  runner: "local",
+  specs: ["./test/specs/android/*.js"],
+  exclude: [],
+  maxInstances: 10,
+  services: ["appium"],
+  hostname: "localhost",
+  port: 4723,
+  path: "/wd/hub",
 
-    capabilities: [{
-        'appium:platformName': 'Android',
-        'appium:deviceName': 'Pixel 9',
-        'appium:platformVersion': '16.0',
-        'appium:automationName': 'UiAutomator2',
-        'appium:app': path.join(process.cwd(), 'app/android/SauceLabs.apk'),
-        "appium:appWaitActivity": "*",
-        "appium:autoGrantPermissions": true,
-        "appium:noReset": true
-    }],
+  capabilities: [
+    {
+      "appium:platformName": "Android",
+      "appium:deviceName": "Pixel_9",
+      "appium:platformVersion": "16.0",
+      "appium:automationName": "UiAutomator2",
+      "appium:app": path.join(process.cwd(), "app/android/SauceLabs.apk"),
+      "appium:appWaitActivity": "*",
+      "appium:autoGrantPermissions": true,
+      "appium:noReset": true,
+    },
+    {
+      "appium:platformName": "Android",
+      "appium:deviceName": "Pixel_8",
+      "appium:platformVersion": "14.0",
+      "appium:automationName": "UiAutomator2",
+      "appium:app": path.join(process.cwd(), "app/android/SauceLabs.apk"),
+      "appium:appWaitActivity": "*",
+      "appium:autoGrantPermissions": true,
+      "appium:noReset": true,
+    },
+  ],
 
-    logLevel: 'info',
-    bail: 0,
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    framework: 'mocha',
-    reporters: ['spec'],
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000
-    }
+  logLevel: "info",
+  bail: 0,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  framework: "mocha",
+  reporters: ["spec"],
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
 };
